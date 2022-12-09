baee7d666721cd833bf2310133efe246cd1d81e5
padtod1505 <rogelio-1505@hotmail.com>
2022-12-09 01:10:23 -0600

    archivo de practica

---
+++ text.js
@@ -0,0 +1,9 @@
+function deEuroAdolar(euro){
+    //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
+    //como parámetro un número de euros y calcule el cambio en dólares.
+    //Escribe tu código aquí
+    var dolar = euro * 1.2;
+    console.log(dolar)
+    
+  }
+  deEuroAdolar(100)
